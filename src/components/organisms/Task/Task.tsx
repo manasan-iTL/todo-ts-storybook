d426e5ce6030e869e9533a9fa5a578f7171d6c5f
manasan-iTL <daiyaofssh2020@gmail.com>
2022-06-27 22:48:27 +0900

    [add&update] スタイルの修正、フィルター機能、ソート機能の実装

--- src/components/organisms/Task/Task.tsx
+++ src/components/organisms/Task/Task.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import './Task.css'
 import { Button, TaskText, StarIcon } from "../..";
+import {IconProps} from '../../types/types'
 
-type Props = {
-    pinned:boolean,
-    changePinned: () => void
+type Props = IconProps & {
+    onFinishTask: () => void,
+    taskText: string,
 }
 
 const Task: React.FC<Props> = (props) => {
 
-    const handleClick = () => {
-        console.log("Clicked Finish")
-    }
-
     return (
         <div className="task">
-            <StarIcon pinned={props.pinned} size={2}  changePinned={props.changePinned}/>
-            <TaskText text="英語の課題" />
-            <Button text="Finish" addTask={ false } onClick={handleClick} />
+            <StarIcon id = {props.id} pinned={props.pinned} size={2}  changePinned={(e)=> props.changePinned(e)}/>
+            <TaskText text={props.taskText} />
+            <Button text="Finish" addTask={ false } onClick={props.onFinishTask} />
         </div>
     )
 }
