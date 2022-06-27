8078c48beb15ebfba2c6f5499c8b546ce7ab7355
manasan-iTL <daiyaofssh2020@gmail.com>
2022-06-27 22:53:17 +0900

    [add&update] モック用データの追加とバックグランドカラーのスタイル修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { TopPage } from './components/templates';
+import { Page } from './components';
+import './index.css'
+import { item } from './components/types/types';
+
+const items: item[] = [
+  {id: "1", task: "英語の課題", finish: false, pinned: false }, 
+  {id: "2", task: "プログラミングの課題", finish: true, pinned: false },
+  {id: "3", task: "数学の課題", finish: false, pinned: false },
+  {id: "4", task: "筋トレ", finish: true, pinned: false },
+  {id: "5", task: "買い物", finish: false, pinned: true },
+  {id: "6", task: "掃除", finish: true, pinned: true },
+]
+
+const onClick = () => {
+  console.log("Clicked")
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <TopPage />
+    <Page items={items} onFinishTask = {() => onClick()}/>
   </React.StrictMode>
 );
 
