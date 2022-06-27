b4f49b3b31b71bc75cc4e29994936e72ad270854
manasan-iTL <daiyaofssh2020@gmail.com>
2022-06-27 22:44:55 +0900

    [add&update] 型定義の修正とスタイル変更

--- src/components/atomics/Input/InputTask.tsx
+++ src/components/atomics/Input/InputTask.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import styled from './Input.module.css'
+import { InputProps } from '../../types/types'
 
-const InputTask: React.FC<{}> = (props) => {
+const InputTask: React.FC<InputProps> = (props) => {
     return (
-        <input type="text" />
+        <input  type="text"
+                placeholder="タスクを入力してください" 
+                className={styled.input} 
+                value= {props.value} 
+                onChange={(e) => props.changeInputValue(e)} />
     )
 }
 
