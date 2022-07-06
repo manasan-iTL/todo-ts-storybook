d978e2f851322e616fb00b44eb2250dcba1bcabe
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-06 13:32:48 +0900

    [change] エントリーファイルを修正

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
 export {default as TitleText} from './atomics/Texts/TitleText'
 export {default as TaskText} from './atomics/Texts/TaskText'
-export {default as AddTaskButton} from './atomics/Buttons/AddTaskButton'
-export {default as FinishTaskButton} from './atomics/Buttons/FinishTaskButton'
 export {default as Button} from './atomics/Buttons/Button'
 export {default as Tab} from './atomics/Tabs/Tab'
 export {default as StarIcon} from './atomics/Icons/StarIcon'
