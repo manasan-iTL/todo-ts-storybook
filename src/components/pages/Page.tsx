e85d34492b98b367d15cfce06c2120ad31dec3df
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-06 14:11:30 +0900

    [change] onAddTask()とchangePinned()の処理を簡略化

--- src/components/pages/Page.tsx
+++ src/components/pages/Page.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {TopPage} from "../"
+import { TopPage } from "../"
 import { item } from "../types/types";
 
 /* 外部APIのURL   */
@@ -11,7 +11,7 @@ const Page: React.FC = () => {
     /*  　　　外部APIとの通信用の関数　axiosをラップする関数 　　　*/
 
     //タスクを追加するAPIとの通信を行う
-    const sendTask = async (item:item) => {
+    const sendTask = async (item: item) => {
         const response = await axios.post(url, item)
         return response
     }
@@ -19,11 +19,12 @@ const Page: React.FC = () => {
     // タスクを変更するAPIと通信を行う。
     // 今回は、タスクをピン付けするか・タスクが完了したかを変更するAPIと通信を行う。
 
-    const sendModifyTask = async (taskId:string, taskKey:string, taskValue:string|boolean) => {
+    const sendModifyTask = async (taskId: string, taskKey: string, taskValue: string | boolean) => {
         const response = await axios.patch(url, {
-            id:taskId,
-            key:taskKey,
-            value:taskValue})
+            id: taskId,
+            key: taskKey,
+            value: taskValue
+        })
         console.log(response)
         return response
     }
@@ -48,43 +49,49 @@ const Page: React.FC = () => {
     // openを更新するsetValueのラップ関数
     const changeTab = () => {
         setOpen(!open)
-    } 
+    }
 
 
     /*       
-        複雑なロジックを含むState更新用の関数
+        複数のロジックを含むState更新用の関数
         １　ユーザのアクションによって変化したitems内のitemオブジェクトを抽出
         ２　変化した値を更新
         ３　変化した情報をDBに保存するための関数を実行　上で定義したAPI通信用の関数を実行
         ４　itemsを更新する
-        　　　　　 */
+              */
 
     // ピンをつけたかどうかを、items.pinnedで管理しており、それを更新する関数
 
     const changePinned = (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) => {
 
         // クリックされたDOMのidによって、更新するオブジェクトとそうてないオブジェクトを抽出
-        // id: pinnedO(O = item.id) 末尾の数字とitem.idは一致している
+        // id: iconO(O = item.id) 末尾の数字とitem.idは一致している
 
-        const newObj = items.filter(item => item.id === e.currentTarget.id.slice(-1))[0]
-        const oldArray = items.filter(item => item.id !== e.currentTarget.id.slice(-1))
-
-        // 値を更新
-        newObj.pinned = !(newObj.pinned)
-
-        // API通信
-        sendModifyTask(newObj.id, "pinned" ,newObj.pinned )
-
-        // 新たに更新する配列を生成＆ソートを行い、元の配列と同じ順番にする
-        const orderArray = [...oldArray, newObj].sort((first, second) => {
-            if(first.id > second.id) {return 1}
-            else if (first.id < second.id) {return -1}
-            else {return 0}
-        })
-
-        // ステートを更新する
-        setItems(orderArray)
+        setItems(items.map(item => {
+            if (item.id === e.currentTarget.id.slice(-1)) {
+                sendModifyTask(item.id, "pinned", !item.pinned)
+                return { ...item, pinned: !item.pinned }
+            } else {
+                return item
+            }
+        }))
     }
+
+
+    // タスクを完了する処理
+    const onFinishTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+
+        // 処理自体はchengePinned()と同じ　共通化しても良いかも
+        setItems(items.map(item => {
+            if (item.id === e.currentTarget.id.slice(-1)) {
+                sendModifyTask(item.id, "finish", !item.finish)
+                return { ...item, finish: !item.finish }
+            } else {
+                return item
+            }
+        }))
+    }
+
 
     // タスクを追加する処理
 
@@ -92,7 +99,7 @@ const Page: React.FC = () => {
         e.preventDefault()
 
         // タスクの生成
-        const newTask:item = {
+        const newTask: item = {
             id: String(items.length + 1),
             task: value,
             finish: false,
@@ -104,7 +111,7 @@ const Page: React.FC = () => {
             alert("文字を入力してください")
             setValue("")
         } else {
-             // API通信
+            // API通信
             sendTask(newTask)
 
             // ステートの更新
@@ -112,19 +119,6 @@ const Page: React.FC = () => {
             setValue("")
         }
 
-    }
-
-
-    // タスクを完了する処理
-    const onFinishTask = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    
-        // changePinned関数と同じ処理
-        const newObj = items.filter(item => item.id === e.currentTarget.id.slice(-1))[0]
-        const oldArray = items.filter(item => item.id !== e.currentTarget.id.slice(-1))
-        newObj.finish = !(newObj.finish)
-        sendModifyTask(newObj.id, "finish", newObj.finish )
-        const orderArray = [...oldArray, newObj]
-        setItems(orderArray)
     }
 
 
@@ -140,15 +134,15 @@ const Page: React.FC = () => {
 
 
     return (
-        <TopPage 
+        <TopPage
             items={items}
-            value={value} 
+            value={value}
             open={open}
-            changeInputValue = {(e) => changeInputValue(e)} 
-            changePinned = {(e) => changePinned(e)}
+            changeInputValue={(e) => changeInputValue(e)}
+            changePinned={(e) => changePinned(e)}
             changeTab={changeTab}
-            onAddTask = {(e) => onAddTask(e)}
-            onFinishTask = {(e) => onFinishTask(e)}/>
+            onAddTask={(e) => onAddTask(e)}
+            onFinishTask={(e) => onFinishTask(e)} />
     )
 }
 
