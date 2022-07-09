74a58cf7ed28336ee107141c9504c25de2984c7e
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-09 22:50:40 +0900

    [add] filterSliceの作成

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import todoReducer from '../features/todos/todoSlice'
+import filterReducer from '../features/filters/filterSlice'
 
 export const store = configureStore({
     reducer: {
-        todo: todoReducer
+        todo: todoReducer,
+        filter: filterReducer
     },
 })
 
