c97bea78e107271bbdb65b5232b85d29ac774875
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-12 14:58:32 +0900

    [change] Propsの変更に伴い、不要な部分を削除

--- src/stories/atomics/Tabs/Tab.stories.tsx
+++ src/stories/atomics/Tabs/Tab.stories.tsx
@@ -13,13 +13,13 @@ const Template: ComponentStory<typeof Tab> = (args) => <Tab {...args} />
 export const ActiveTab = Template.bind({})
 ActiveTab.args = {
     text: '完了',
-    open: true,
+    // open: true,
     // onClick: action("Clicked")
 }
 
 export const DisActiveTab = Template.bind({})
 DisActiveTab.args = {
     text: '未完了',
-    open: false,
+    // open: false,
     // onClick: action("Clicked")
 }
