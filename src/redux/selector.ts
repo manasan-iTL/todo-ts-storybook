20f644717d8b6f6c8d0de14494b8fd0ff1103ec5
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-12 14:52:47 +0900

    [change] タスクをフィルター＆並び変えるセレクターを作成

---
+++ src/redux/selector.ts
@@ -0,0 +1,20 @@
+import { FilterState, item } from "./types";
+
+export const FilterTodosByTab = (
+    todos: item[],
+    finish: FilterState
+) : item[] => {
+    if (finish.open) {
+        // 進行中か完了しているかでタスクを分ける
+        const sortTaskList = todos.filter((todo) => todo.finish)
+        // ピン付けされたタスクを上位に表示させるために、別々に抽出して並び替えている。
+        const piinedTasks = sortTaskList.filter((order) => order.pinned)
+        const unPinnedTasks = sortTaskList.filter((order) => !(order.pinned))
+        return [...piinedTasks, ...unPinnedTasks]
+    } else {
+        const sortTaskList = todos.filter((todo) => !todo.finish)
+        const piinedTasks = sortTaskList.filter((order) => order.pinned)
+        const unPinnedTasks = sortTaskList.filter((order) => !(order.pinned))
+        return [...piinedTasks, ...unPinnedTasks]
+    }
+}
