ad1bc957ad816bfc85cd46688551502a4c94cc13
manasan-iTL <daiyaofssh2020@gmail.com>
2022-07-12 14:53:30 +0900

    [change] 外部APIとの通信を行う関数をまとめた。

---
+++ src/api/FetchApi.ts
@@ -0,0 +1,21 @@
+import axios from "axios"
+import { item } from "../components/types/types"
+
+const url = "https://wjn8pv712f.execute-api.ap-northeast-1.amazonaws.com/dev/tasks"
+
+export const fetchTodos = async () => {
+    const response = await axios.get(url)
+    return response.data
+}
+
+export const sendTodoApi = async (item: item) => {
+    await axios.post(url, item)
+}
+
+export const modifyTodoApi = async (taskId: string, taskKey: string, taskValue: boolean) => {
+    await axios.patch(url, {
+        id: taskId,
+        key: taskKey,
+        value: taskValue
+    })
+}
